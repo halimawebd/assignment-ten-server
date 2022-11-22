34a838ba1418a127ad7039e08cfde1b3be8368d4
halimawebd <halimawebd@gmail.com>
2022-11-22 10:23:34 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+app.use(cors())
+const port = process.env.port || 5000
+
+const categories = require('./data/category.json')
+const courses = require('./data/course.json')
+
+app.get('/', (req, res)=>{
+    res.send('Courses API is running')
+})
+app.get('/category-course', (req, res)=>{
+    res.send(categories)
+})
+app.get('/course-category', (req, res)=>{
+    res.send(courses)
+})
+app.get('/courseData/:id', (req, res)=>{
+    const id = req.params.id ;
+    const selectedId = courses.find(c => c.id === id) 
+    res.send(selectedId)
+})
